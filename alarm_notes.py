2db3effee99f124c046cba08fc1af958a0add0b1
mcriqui <maggie.criqui@gmail.com>
2015-01-20 09:14:44 -0500

    Update alarm_notes.py

--- alarm_notes.py
+++ alarm_notes.py
@@ -10,13 +10,15 @@ print("Hello, " + name)
 
 alarm_HH = raw_input("Enter the hour you want to wake up at")
 alarm_MM = raw_input("Enter the minute you want to wake up at")
-
 alarm_time = "{0}:{1}".format(alarm_HH, alarm_MM)
 
+#converts raw input into datetime format
 time_obj = datetime.strptime(alarm_time, '%I:%M')
 
+#establishes how much we'll adjust the time if there is a delay. In our final script, this will be in the 'if there is a delay'...
 change_in_time = timedelta(minutes=20)
 
+#creates the new time the person wants to wake up
 new_time = time_obj - change_in_time
 
 
